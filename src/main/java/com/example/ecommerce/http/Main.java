eccf9ad80684819be93842f3394dfa803df0c142
Jorgc <condori.jrge@gmail.com>
2022-10-08 18:36:03 -0300

    Update Procfile

--- src/main/java/com/example/ecommerce/http/Main.java
+++ src/main/java/com/example/ecommerce/http/Main.java
@@ -5,7 +5,7 @@ import com.example.ecommerce.core.Core;
 public class Main {
     public static void main(String[] args) {
         Core core = new Core();
-        HttpApplication httpApplication = new HttpApplication(Integer.valueOf(System.getProperty("server.port")), core);
+        HttpApplication httpApplication = new HttpApplication(Integer.valueOf(System.getenv("PORT")), core);
         httpApplication.start();
     }
 }
