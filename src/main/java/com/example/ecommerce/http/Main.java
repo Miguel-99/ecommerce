7feef75abec8f1cf6d63701728a493ade6b6c963
Jorgc <condori.jrge@gmail.com>
2022-10-08 17:56:32 -0300

    Update Procfi

--- src/main/java/com/example/ecommerce/http/Main.java
+++ src/main/java/com/example/ecommerce/http/Main.java
@@ -5,7 +5,7 @@ import com.example.ecommerce.core.Core;
 public class Main {
     public static void main(String[] args) {
         Core core = new Core();
-        HttpApplication httpApplication = new HttpApplication(8080, core);
+        HttpApplication httpApplication = new HttpApplication(Integer.valueOf(System.getProperty("server.port")), core);
         httpApplication.start();
     }
 }
